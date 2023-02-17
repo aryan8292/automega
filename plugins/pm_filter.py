027c6df0e04c9321dab6fa726240276a12b2012e
aryan8292 <94910943+aryan8292@users.noreply.github.com>
2023-02-17 23:08:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -435,7 +435,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton('➕↖️ Add Me To Your Groups ↗️➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('⚡ Developer ⚡', url=f"https://t.me/starkownerbot"),
+            InlineKeyboardButton('⚡ Developer ⚡', url=f"https://t.me/ary_botz"),
             ],[
              InlineKeyboardButton('🙆🏻 Help 🦾', callback_data='help'),
             InlineKeyboardButton('♥️ About ♥️', callback_data='about')
