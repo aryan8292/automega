2e0bb8467075333da9e76fd93dea071b9d3c59d0
aryan8292 <94910943+aryan8292@users.noreply.github.com>
2023-08-14 11:03:29 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,7 +383,7 @@ async def get_shortlink(link):
         https = "https"
         link = link.replace("http", https)
 
-    url = f'http://GreyMatterslinks.in/api'
+    url = f'GreyMatterslinks.in/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
